8bfc82ec83d4b54aa754397c8b99495dcbafd92d
myrtics <30813957+myrtics@users.noreply.github.com>
2018-07-19 10:02:48 -0400

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,6 +1,5 @@
 exports.run = (client, message, args) => {
-
-    message.delete();
+    
     message.channel.send(`${message.author} You must be really bored if you are talking to a bot smh. It's ${message.createdTimestamp} go do something useful.`)
 
 }
