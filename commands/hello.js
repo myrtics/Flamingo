6399d3710ac554d73a597bcd7c5044831051b2c5
myrtics <30813957+myrtics@users.noreply.github.com>
2018-07-19 10:30:46 -0400

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,5 +1,4 @@
 exports.run = (client, message, args) => {
     
-    message.channel.send(`${message.author} You must be really bored if you are talking to a bot smh. It's ${message.createdAt} go do something useful.`)
-
+    message.channel.send(`${message.author}, You must be really bored if you are talking to a bot smh.
 }
