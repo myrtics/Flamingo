b2ce1f4b4503b7d9436c7a49d853f999aba7b567
myrtics <30813957+myrtics@users.noreply.github.com>
2018-07-19 09:54:18 -0400

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
 
     message.delete();
-    message.channel.send(`${client.author} You must be really bored if you are talking to a bot smh.`)
+    message.channel.send(`${message.author} You must be really bored if you are talking to a bot smh.`)
 
 }
